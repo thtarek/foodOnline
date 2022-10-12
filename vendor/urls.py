f5387ddab5ac4e51270672c726df54ff7aee0e37
tarek-hossain <tarekth.me@gmail.com>
2022-10-12 23:27:04 +0600

    menu builder - category CRUD functionalities

--- vendor/urls.py
+++ vendor/urls.py
@@ -5,4 +5,11 @@ urlpatterns = [
     path('', AccountViews.vendorDashboard, name='vendor'),
     path('profile/', views.vendorProfile, name="vendor-profile"),
     path('menu-builder', views.menu_builder, name="menu_builder"), 
+    path('menu-builder/category/<int:pk>', views.fooditems_by_category, name="fooditems_by_category"),
+
+    #category CRUD
+    path('menu-builder/category/add/', views.add_category, name='add_category'),
+    path('menu-builder/category/edit/<int:pk>/', views.edit_category, name='edit_category'),
+    path('menu-builder/category/delete/<int:pk>/', views.delete_category, name='delete_category')
+
 ]
