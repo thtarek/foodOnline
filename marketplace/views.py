f1a72a912360ada80f7a05d92493438029b558b2
tarek-hossain <tarekth.me@gmail.com>
2022-10-20 23:28:56 +0600

    show cart items in the cart

--- marketplace/views.py
+++ marketplace/views.py
@@ -95,4 +95,8 @@ def decrease_cart(request, food_id):
         return JsonResponse({'status': 'login_required', 'message':'Please login to continue.'})
 
 def cart(request):
-    return render(request, 'marketplace/cart.html')
+    cart_items = Cart.objects.filter(user=request.user)
+    context = {
+        'cart_items':cart_items
+    }
+    return render(request, 'marketplace/cart.html', context)
