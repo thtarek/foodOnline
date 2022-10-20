ae963a67abdb9aa47f7f7ed06984d359e43191dd
tarek-hossain <tarekth.me@gmail.com>
2022-10-20 22:41:17 +0600

    cart url setup

--- foodOnlineMain/urls.py
+++ foodOnlineMain/urls.py
@@ -20,10 +20,13 @@ from . import views
 
 from django.conf import settings
 from django.conf.urls.static import static
+from marketplace import views as marketplaceViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('accounts/', include('accounts.urls')),
     path('marketplace/', include('marketplace.urls')),
+    #CART
+    path('cart/', marketplaceViews.cart, name='cart')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- marketplace/views.py
+++ marketplace/views.py
@@ -93,3 +93,6 @@ def decrease_cart(request, food_id):
             return JsonResponse({'status': 'Failed', 'message':'Invalid request'})
     else:
         return JsonResponse({'status': 'login_required', 'message':'Please login to continue.'})
+
+def cart(request):
+    return render(request, 'marketplace/cart.html')
