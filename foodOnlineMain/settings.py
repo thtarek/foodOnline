e6fea7285c060dd8d94c5e8079283044d64ce465
tarek-hossain <tarekth.me@gmail.com>
2022-11-07 21:42:03 +0600

    load cover and profile picture

--- foodOnlineMain/settings.py
+++ foodOnlineMain/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 'accounts.context_processors.get_google_api',
                 'marketplace.context_processors.get_cart_counter',
                 'marketplace.context_processors.get_cart_amount',
+                'accounts.context_processors.get_user_profile',
             ],
         },
     },
