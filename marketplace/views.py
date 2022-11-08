e29a24b15fb0d337a3fe2ad61ee3490c42bfbacd
tarek-hossain <tarekth.me@gmail.com>
2022-11-09 00:25:52 +0600

    prepopulate billing form

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,6 +1,7 @@
 
 from datetime import date, datetime
 from django.shortcuts import render, get_object_or_404, HttpResponse
+from accounts.models import UserProfile
 from marketplace.context_processors import get_cart_counter, get_cart_amount
 from orders.forms import orderForm
 from vendor.models import OpeningHour, Vendor
@@ -127,13 +128,27 @@ def delete_cart_item(request, cart_id):
                 return JsonResponse({'status': 'Failed', 'message':'Cart item does not exist'})
         else:
             return JsonResponse({'status': 'Failed', 'message':'Invalid request'})
-
+            
+@login_required(login_url='login')
 def checkout(request):
     cart_items = Cart.objects.filter(user=request.user).order_by('created_at')
     cart_count = cart_items.count()
     if cart_count <= 0:
         return redirect('marketplace')
-    form = orderForm()
+    user_profile = UserProfile.objects.get(user=request.user)
+    default_values = {
+        'first_name': request.user.first_name,
+        'last_name': request.user.last_name,
+        'phone': request.user.phone_number,
+        'email': request.user.email,
+        'address': user_profile.address,
+        'country': user_profile.country,
+        'state': user_profile.state,
+        'city': user_profile.city,
+        'pin_code': user_profile.pin_code,
+        
+    }
+    form = orderForm(initial=default_values)
     context = {
         'form': form,
         'cart_items' : cart_items,
