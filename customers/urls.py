e7563ec52a009cc7ba811bfad681bb695fc83cf2
tarek-hossain <tarekth.me@gmail.com>
2022-11-17 14:55:00 +0600

    order detail setup

--- customers/urls.py
+++ customers/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', AccountViews.customerDashboard, name='customer'),
     path('profile/', views.cprofile, name='cprofile'),
     path('my-orders/', views.my_orders, name='customer_my_orders'),
+    path('order-detail/<int:order_number>/', views.order_detail, name='order_detail'),
 ]
--- customers/views.py
+++ customers/views.py
@@ -3,7 +3,8 @@ from django.contrib.auth.decorators import login_required
 from accounts.forms import UserProfileForm, UserInfoForm
 from accounts.models import UserProfile
 from django.contrib import messages
-from orders.models import Order
+from orders.models import Order, OrderedFood
+import simplejson as json
 
 # Create your views here.
 @login_required(login_url='login')
@@ -37,3 +38,21 @@ def my_orders(request):
         'orders': orders,
     }
     return render(request, 'customers/my_orders.html', context)
+
+def order_detail(request, order_number):
+     try:
+        order = Order.objects.get(order_number=order_number, is_ordered=True)
+        ordered_food = OrderedFood.objects.filter(order=order)
+        subtotal = 0
+        for item in ordered_food:
+            subtotal += (item.price * item.quantity)
+        tax_data = json.loads(order.tax_data)
+        context = {
+            'order': order,
+            'ordered_food': ordered_food,
+            'subtotal': subtotal,
+            'tax_data': tax_data,
+        }
+        return render(request, 'customers/order_detail.html', context)
+     except:
+        return redirect('customer')
